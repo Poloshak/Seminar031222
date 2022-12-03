94ae5e956bf32036538024508f7bc6723d9c5857
Poloshak <e.l.fomenko@yandex.ru>
2022-12-03 20:44:02 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"{num1},{num2}->Кратно");
+}
+else
+{
+    Console.WriteLine($"{num1},{num2}->не кратно, остаток {num1 % num2}");
+}
